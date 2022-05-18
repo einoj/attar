8ca2d28444ae87d5985715a4a68b301bb704ea44
Eino J. Oltedal <ejo@datarespons.no>
2022-05-18 23:50:31 +0200

    Add nested case for 0xFXXX opcodes

--- åttår.nim
+++ åttår.nim
@@ -1,6 +1,7 @@
 import streams
 import std/strformat
 import std/endians
+import std/bitops
 
 type chip8 = object
   ram: array[4096, uint16]
@@ -20,9 +21,21 @@ proc loadrom(romname: string, machine: ref chip8) =
 
 proc alu(machine: ref chip8) =
   while machine.pc<4096:
-    case machine.ram[machine.pc]
+    var n: uint16 = machine.ram[machine.pc]
+    case n:
+      of 0x00E0:
+        echo fmt"display_clear"
+      of 0x00EE:
+        echo fmt"return"
       of 0x1000..0x1fff:
         echo fmt"jump_to_addr 0x{machine.ram[machine.pc]:4x}"
+      of 0xf000..0xffff:
+        var v = 0x00ff'u16
+        n.mask(v)
+        case n:
+          of 0x33:
+            echo fmt "BCD"
+          else: discard
       else: discard
     machine.pc += 1
 
