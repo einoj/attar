b1accee532830f0b6f15fdee856661e75721250c
Eino J. Oltedal <ejo@datarespons.no>
2022-05-21 21:08:19 +0200

    Implement the draw function

--- opfuncs.nim
+++ opfuncs.nim
@@ -8,6 +8,7 @@ type chip8* = object
   variables*: array[16, uint8]
   stack: seq[uint16]
   i: uint16
+  framebuff: array[64, array[32, uint16]]
 
 proc jump_to_addr*(machine: ref chip8, address: uint16) =
   echo fmt"jump to 0x{address:3x}"
@@ -30,3 +31,16 @@ proc return_subroutine*(machine: ref chip8) =
 proc set_pointer_reg*(machine: ref chip8, address: uint16) =
   machine.i = address
 
+proc draw*(machine: ref chip8, lower: uint16) =
+  var vx = lower
+  var vy = lower
+  var n = lower
+  vx.bitslice(8..11)
+  vy.bitslice(4..7)
+  n.bitslice(0..3)
+  var pointr = machine.i
+  for row in machine.variables[vx]..8:
+    for col in machine.variables[vy]..n:
+      machine.framebuff[row][col] = machine.framebuff[row][col] xor machine.ram[pointr]
+      pointr += 1
+    
