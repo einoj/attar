d70ee0674a0c7fe0a39c05bf0caec3307333d403
Eino J. Oltedal <einoju@gmail.com>
2022-10-20 22:57:48 +0200

    Fix debug printing for opcodes

--- attar/opfuncs.nim
+++ attar/opfuncs.nim
@@ -11,24 +11,32 @@ type chip8* = object
   framebuf*: array[64*32, uint8]
 
 proc jump_to_addr*(machine: ref chip8, address: uint16) =
-  echo fmt"jump to 0x{address:3x}"
+  when not defined(release):
+    echo fmt"jump to 0x{address:3x}"
   machine.pc = address
 
 proc set_variable*(machine: ref chip8, lower: uint16) =
   var variable = lower
   variable.bitslice(8..11)
   var value = uint8(lower)
-  echo fmt"set variable 0x{variable:x} = 0x{value:x}"
+  when not defined(release):
+    echo fmt"set variable 0x{variable:x} = 0x{value:x}"
   machine.variables[variable] = value
 
 proc call_subroutine*(machine: ref chip8, address: uint16) =
+  when not defined(release):
+    echo fmt"return_subroutine"
   machine.stack.add(machine.pc)
   jump_to_addr(machine, address)
 
 proc return_subroutine*(machine: ref chip8) =
+  when not defined(release):
+    echo fmt"return_subroutine"
   machine.pc =  machine.stack.pop
 
 proc set_pointer_reg*(machine: ref chip8, address: uint16) =
+  when not defined(release):
+    echo fmt"set_pointer_reg to 0x{address:3x}"
   machine.i = address
 
 proc display_clear*(machine: ref chip8) =
